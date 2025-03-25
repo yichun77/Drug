a5054081298613d8d9471d3e58f2101b51925cc3
adayc77 <adayc88@gmail.com>
2025-03-25 16:37:19 +0800

    EDA/data cleaning Updates

--- DA_data/new_data/process_new_data.py
+++ DA_data/new_data/process_new_data.py
@@ -6,13 +6,13 @@ import pandas as pd
 import pyreadstat
 
 # quatraiset of Hastie's "quadratic model"
-df, meta = pyreadstat.read_sas7bdat(r"C:\Users\adayc\Desktop\code\Drug\DA_data\diabetes2.sas7bdat")
+df, meta = pyreadstat.read_sas7bdat(r"C:\Users\adayc\Desktop\code\Drug\DA_data\new_data\diabetes2.sas7bdat")
 print(df.head())  
 print(meta.column_names)  
 print(meta.column_labels)  
 
 # original data
-dftxt = pd.read_csv(r"C:\Users\adayc\Desktop\code\Drug\DA_data\diabetesrwrite1.txt", sep="\s+")
+dftxt = pd.read_csv(r"C:\Users\adayc\Desktop\code\Drug\DA_data\new_data\diabetesrwrite1.txt", sep="\s+")
 num_rows, num_columns = dftxt.shape
 print(dftxt.head())
 print(f"rows: {num_rows}")

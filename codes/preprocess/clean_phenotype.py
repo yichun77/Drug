dff7c9d901cc3465bb0088c9b5af05bbc36f6f2d
adayc77 <adayc88@gmail.com>
2025-03-27 10:36:42 +0800

    EDA finished

---
+++ codes/preprocess/clean_phenotype.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load the phenotype data
+phenotype = pd.read_excel(r'C:\Users\adayc\Desktop\code\Drug\DA_data\DA_phenotype.xlsx')

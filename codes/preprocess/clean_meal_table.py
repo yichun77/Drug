cf9a445d7c61bfeea3254ef40d0030bb48a8264a
adayc77 <adayc88@gmail.com>
2025-03-27 17:36:26 +0800

    data cleaning

---
+++ codes/preprocess/clean_meal_table.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load the data
+dfc = pd.read_excel(r'C:\Users\adayc\Desktop\code\Drug\DA_data\DA_daily_food_clean.xlsx')
+dm = pd.read_excel(r'C:\Users\adayc\Desktop\code\Drug\DA_data\DA_daily_meal.xlsx')
+
+# The food amount of DA163 was always 0, so we delete the DA163 data
+# And we also delete DA148 and DA196 because they have missing data
+excluded_ids = ['DA163', 'DA148', 'DA196']
+dfc_new = dfc[~dfc['Sampleid'].isin(excluded_ids)]
+dm_new = dm[~dm['Sampleid'].isin(excluded_ids)]
+
+# Save the cleaned data
+dfc_new_output_path = r'C:\Users\adayc\Desktop\code\Drug\DA_data\filtered_DA_daily_food_clean.xlsx'
+dm_new_output_path = r'C:\Users\adayc\Desktop\code\Drug\DA_data\filtered_DA_daily_meal.xlsx'
+dfc_new.to_excel(dfc_new_output_path, index=False)
+dm_new.to_excel(dm_new_output_path, index=False)
+
+print(f"Filtered dfc_new saved to {dfc_new_output_path}")
+print(f"Filtered dm_new saved to {dm_new_output_path}")
--- codes/preprocess/clean_phenotype.py
+++ codes/preprocess/clean_phenotype.py
@@ -4,3 +4,27 @@ import seaborn as sns
 
 # Load the phenotype data
 phenotype = pd.read_excel(r'C:\Users\adayc\Desktop\code\Drug\DA_data\DA_phenotype.xlsx')
+
+# Remove specific columns with personal information
+phenotype = phenotype.drop(columns=['brigade', 'IDNUM']) # add more columns if needed
+
+# Separate rows with "type" 1: DA; 2: Healthy
+type1 = phenotype[phenotype['type'] == 1]
+type2 = phenotype[phenotype['type'] == 2]
+type1.columns = phenotype.columns
+type2.columns = phenotype.columns
+
+# Rename the healthy group ID
+type2['ID'] = [f"HP{index+1:03}" for index in range(len(type2))]
+
+# Remove rows with specific IDs in DA group (data missing or not reliable)
+excluded_ids = ['DA148', 'DA163', 'DA196']
+type1 = type1[~type1['ID'].isin(excluded_ids)]
+
+# Save the cleaned data
+da_output_path = r'C:\Users\adayc\Desktop\code\Drug\DA_data\only_DA_phenotype.xlsx'
+healthy_output_path = r'C:\Users\adayc\Desktop\code\Drug\DA_data\healthy_phenotype.xlsx'
+type1.to_excel(da_output_path, index=False)
+type2.to_excel(healthy_output_path, index=False)
+print(f"DA table saved to {da_output_path}")
+print(f"Non-DA table saved to {healthy_output_path}")
